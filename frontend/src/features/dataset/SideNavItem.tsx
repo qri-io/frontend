b9509cf16a7f32399abb4d05abb7edf71c741bbf
Chris Whong <chris.m.whong@gmail.com>
2021-01-29 13:05:49 -0500

    feat: run bar improvements and tooltips

--- frontend/src/features/dataset/SideNavItem.tsx
+++ frontend/src/features/dataset/SideNavItem.tsx
@@ -1,20 +1,39 @@
 import React from 'react'
 import { Link, useLocation } from 'react-router-dom';
+import ReactTooltip from 'react-tooltip'
 
 import Icon from '../../chrome/Icon';
 
 export interface SideNavItemProps {
+  id: string;
   icon: string;
   to: string;
+  tooltip?: React.ReactNode
 }
 
-const SideNavItem: React.FC<SideNavItemProps> = ({ icon, to }) => {
+const SideNavItem: React.FC<SideNavItemProps> = ({ id, icon, to, tooltip }) => {
   const { pathname } = useLocation();
   const active = pathname === to
   return (
       <Link to={to} className=''>
-        <div className={`text-gray-500 text-center h-10 w-10 m-3 rounded py-2 hover:bg-gray-300 transition-all duration-200 ${active && 'bg-gray-300'}`}>
+        <div
+          className={`text-gray-500 text-center h-10 w-10 m-3 rounded py-2 hover:bg-gray-300 transition-all duration-200 ${active && 'bg-gray-300'}`}
+          data-tip
+          data-for={id}
+        >
           <Icon icon={icon} />
+          {tooltip && (
+            <ReactTooltip
+              id={id}
+              place='right'
+              effect='solid'
+              offset={{
+                right: 10
+              }}
+            >
+              {tooltip}
+            </ReactTooltip>
+          )}
         </div>
       </Link>
   )
