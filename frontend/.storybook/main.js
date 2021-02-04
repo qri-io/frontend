3d1dcb6ab05de510786d44880e888f85a0eb494d
ramfox <kasey@qri.io>
2021-02-04 00:01:38 -0500

    refactor(`storybook`): add old styles for `ds_components` to storybook
    In order to get the `ds_components` story up and running in a semi-coherent way, I've pulled the styling (except for the fonts), into one file `old_styles.scss` & use webpack to compile and make it available in storybook.
    
    When we transfer these components over to using tailwind for styling, we should remove `src/old_styles.scss`, remove remove 'style-loader', 'css-loader', 'sass-loader', and 'node-sass' from `package.json`, and remove the `old_styles.scss` import from `.storybook/preview.js`

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -27,6 +27,18 @@ module.exports = {
           },
         ],
         include: path.resolve(__dirname, '../'),
+      },
+      // TODO (ramfox): remove when we convert scss to tailwind in `features/ds_components`
+      // also remove 'style-loader', 'css-loader', 'sass-loader', and 'node-sass' from `package.json`
+      {
+        test: '/\.scss$/',
+        use: ['style-loader', 'css-loader', {
+          loader: 'sass-loader',
+          options: {
+            outFile: '../old_styles.css'
+          }
+        }],
+        include: path.resolve(__dirname, '../')
       }
     ],
     config.resolve.extensions.push('.ts', '.tsx');
