efecbd04293ee5b503a1a4a78bbe7239ea8e6fdc
Chris Whong <chris.m.whong@gmail.com>
2021-06-07 18:40:50 -0400

    fix: icon attribute cleanup

--- frontend/src/chrome/icon/SkinnySearch.tsx
+++ frontend/src/chrome/icon/SkinnySearch.tsx
@@ -3,8 +3,8 @@ import CustomIcon, { CustomIconProps } from '../CustomIcon'
 
 const SkinnySearch: React.FC<CustomIconProps> = (props) => (
   <CustomIcon {...props}>
-    <path fillRule="evenodd" clipRule="evenodd" d="M9.79147 18.6668C14.0946 18.6668 17.5829 14.9358 17.5829 10.3334C17.5829 5.73098 14.0946 2 9.79147 2C5.48836 2 2 5.73098 2 10.3334C2 14.9358 5.48836 18.6668 9.79147 18.6668Z" stroke="#9CA3AF" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-    <path d="M20.6996 22L15.2975 16.2222" stroke="#9CA3AF" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+    <path fillRule="evenodd" clipRule="evenodd" d="M9.79147 18.6668C14.0946 18.6668 17.5829 14.9358 17.5829 10.3334C17.5829 5.73098 14.0946 2 9.79147 2C5.48836 2 2 5.73098 2 10.3334C2 14.9358 5.48836 18.6668 9.79147 18.6668Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+    <path d="M20.6996 22L15.2975 16.2222" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
   </CustomIcon>
 )
 
