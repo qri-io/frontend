37b9d2fbfe567c5081094932e41f85f43bde1603
Chris Whong <chris.m.whong@gmail.com>
2021-06-08 13:29:44 -0400

    feat: add onClick prop to IconLink

--- frontend/src/chrome/IconLink.tsx
+++ frontend/src/chrome/IconLink.tsx
@@ -1,21 +1,42 @@
 import React from 'react'
 import ExternalLink from './ExternalLink'
-import Icon from './Icon'
+import Icon, { IconSize } from './Icon'
 
 interface IconLinkProps {
   icon: string
-  link: string
+  link?: string
+  onClick?: () => void
+  size?: IconSize
 }
 
 const IconLink: React.FC<IconLinkProps> = ({
   icon,
-  link
-}) => (
-  <div className='ml-2'>
-    <ExternalLink to={link} className='text-qrinavy hover:text-qrinavy-600'>
-      <Icon icon={icon} size='sm'/>
-    </ExternalLink>
-  </div>
-)
+  link,
+  onClick,
+  size='sm'
+}) => {
+  const iconComponent = <Icon icon={icon} size={size} />
+  const classNames = 'text-qrinavy hover:text-qripink hover:cursor-pointer'
+
+  if (link) {
+    return (
+      <div className='ml-2'>
+        <ExternalLink to={link} className={classNames}>
+          {iconComponent}
+        </ExternalLink>
+      </div>
+    )
+  }
+
+  if (onClick) {
+    return (
+      <div className='ml-2'>
+        <div className={classNames} onClick={onClick}>
+          {iconComponent}
+        </div>
+      </div>
+    )
+  }
+}
 
 export default IconLink
