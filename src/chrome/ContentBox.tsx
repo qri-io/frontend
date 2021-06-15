fb37caa2011df3910515d6c155420a528e35c993
Chris Whong <chris.m.whong@gmail.com>
2021-06-14 22:23:41 -0400

    feat: add optional paddingClassName to ContentBox

--- src/chrome/ContentBox.tsx
+++ src/chrome/ContentBox.tsx
@@ -3,13 +3,15 @@ import classNames from 'classnames'
 
 interface ContentBoxProps {
   className?: string
+  paddingClassName?: string
 }
 
 const ContentBox: React.FC<ContentBoxProps> = ({
   className,
+  paddingClassName = 'py-6 px-8',
   children
 }) => (
-  <div className={classNames('min-height-200 py-6 px-8 rounded-lg bg-white', className)}>
+  <div className={classNames('min-height-200 rounded-lg bg-white', className, paddingClassName)}>
     {children}
   </div>
 )
