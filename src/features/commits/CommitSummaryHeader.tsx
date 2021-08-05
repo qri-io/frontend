7bddf2d28c2ce7f665267634ccd1713879197763
Chris Whong <chris.m.whong@gmail.com>
2021-08-04 23:10:03 -0400

    feat: tweaks to collection table, commit list, commit header

--- src/features/commits/CommitSummaryHeader.tsx
+++ src/features/commits/CommitSummaryHeader.tsx
@@ -20,15 +20,14 @@ const CommitSummaryHeader: React.FC<CommitSummaryHeaderProps> = ({
       <div className='min-height-200 py-4 px-8 rounded-lg bg-white flex'>
         <div className='flex-grow'>
           <div className='text-xs text-gray-400 font-medium mb-2'>Version Info</div>
-          <div className='text-qrinavy text-sm flex items-center mb-2'>
-            <Icon icon='commit' size='sm' className='-ml-2' />
-            <div className='font-medium'>{commitishFromPath(path)}</div>
-            <div className='mx-3 text-gray-400'>|</div>
+          <div className='text-qrinavy font-semibold text-sm flex items-center mb-2'>
             <div className=''>{commit.title}</div>
           </div>
-          <div className='flex items-centern text-xs text-gray-400'>
+          <div className='flex items-center text-xs text-gray-400'>
             <UsernameWithIcon username={dataset.peername} className='mr-3' />
-            <RelativeTimestampWithIcon timestamp={new Date(commit.timestamp)} className='mr-4' />
+            <RelativeTimestampWithIcon className='mr-3' timestamp={new Date(commit.timestamp)} />
+            <Icon icon='commit' size='sm' className='-ml-2' />
+            <div className=''>{commitishFromPath(path)}</div>
           </div>
         </div>
         <div className='flex items-center'>
