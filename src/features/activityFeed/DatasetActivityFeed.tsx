4922c8a21aace82e44b8984ad508dee46fbbe162
Chris Whong <chris.m.whong@gmail.com>
2021-08-26 14:20:27 -0400

    refactor: prevent unnecessary calls to /activity

--- src/features/activityFeed/DatasetActivityFeed.tsx
+++ src/features/activityFeed/DatasetActivityFeed.tsx
@@ -1,8 +1,8 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { QriRef } from '../../qri/ref'
 import useDimensions from 'react-use-dimensions'
 
+import { QriRef } from '../../qri/ref'
 import ActivityList from './ActivityList'
 import { loadDatasetLogs } from './state/activityFeedActions'
 import { newDatasetLogsSelector } from './state/activityFeedState'
@@ -10,6 +10,7 @@ import DatasetFixedLayout from '../dataset/DatasetFixedLayout'
 import { runNow } from '../workflow/state/workflowActions'
 import { selectLatestRun } from '../workflow/state/workflowState'
 import RunNowButton from './RunNowButton'
+import useDidMountEffect from '../../utils/useDidMountEffect'
 
 
 export interface DatasetActivityFeedProps {
@@ -26,12 +27,18 @@ const DatasetActivityFeed: React.FC<DatasetActivityFeedProps> = ({
 
   const [tableContainer, { height: tableContainerHeight }] = useDimensions()
 
+  // TODO(chriswhong): we are already dispatching loadDatsetLogs() in DatasetWrapper
+  // because we need the same activity list for both the commit list and the run log
+  // once we have a top-level dataset metainfo response, this component will need
+  // to call for data on its own
+  // useEffect(() => {
+  //   dispatch(loadDatasetLogs(qriRef))
+  // },[dispatch, qriRef])
 
-  useEffect(() => {
-    dispatch(loadDatasetLogs(qriRef))
-  },[dispatch, qriRef])
 
-  useEffect(() => {
+  // useDidMountEffect behaves like useEffect but does not run on the first render
+  // this will fetch a new activitylist when latestRun.status changes, keeping the run log fresh
+  useDidMountEffect(() => {
     dispatch(loadDatasetLogs(qriRef))
     // eslint-disable-next-line react-hooks/exhaustive-deps
   },[latestRun?.status])
---
+++ src/utils/useDidMountEffect.tsx
@@ -0,0 +1,15 @@
+// useDidMountEffect behaves like useEffect but does not run on the first render
+
+import { useEffect, useRef } from 'react'
+
+const useDidMountEffect = (func: () => void, deps: any[]) => {
+    const didMount = useRef(false)
+
+    useEffect(() => {
+      if (didMount.current) func()
+      else didMount.current = true
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, deps)
+}
+
+export default useDidMountEffect
