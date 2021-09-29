1fc7ffdb5724a62d4026177d8bed2bc4ba67624d
Andriy Borovets <aborovets@integritylab.com>
2021-09-29 17:51:02 +0300

    feat: added css rule to respect new lines in workflow cell outputs

--- src/features/workflow/output/LogLinePrint.tsx
+++ src/features/workflow/output/LogLinePrint.tsx
@@ -9,11 +9,11 @@ const LogLinePrint: React.FC<LogLineProps> = ({ line }) => {
   switch (line.type) {
     case EventLogLineType.ETPrint:
       // TODO (b5) - utilize line.data.lvl to set output colour
-     return <p className='log_line_print_text text-sm text-gray-500'>{line.data.msg}</p>
+     return <p className='log_line_print_text text-sm whitespace-pre text-gray-500'>{line.data.msg}</p>
     case EventLogLineType.ETError:
-     return <p className='text-sm text-dangerred'>{line.data.msg}</p>
+     return <p className='text-sm whitespace-pre text-dangerred'>{line.data.msg}</p>
     default:
-      return <p>{line.data.msg}</p>
+      return <p className='whitespace-pre'>{line.data.msg}</p>
   }
 }
 
