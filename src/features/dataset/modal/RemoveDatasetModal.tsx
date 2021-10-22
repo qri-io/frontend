4bf92aec55c112cbc03a39610d8a77144a7553cd
Andriy Borovets <aborovets@integritylab.com>
2021-10-22 16:36:57 +0300

    fix: added logic that removes ds from collection on remove

--- src/features/dataset/modal/RemoveDatasetModal.tsx
+++ src/features/dataset/modal/RemoveDatasetModal.tsx
@@ -2,24 +2,31 @@ import React from 'react'
 import { useDispatch } from 'react-redux'
 import { AnyAction } from 'redux'
 import { removeDataset } from '../state/datasetActions'
-import { loadCollection } from '../../collection/state/collectionActions'
+import { loadCollection, removeCollectionItem } from '../../collection/state/collectionActions'
 
 import ModalLayout from '../../app/modal/ModalLayout'
 
 export interface RemoveDatasetModalProps {
   username: string,
-  name: string
+  name: string,
+  onDsRemove: () => void
+  afterRemove: () => void
 }
 
-const RemoveDatasetModal: React.FC<RemoveDatasetModalProps> = ({ username, name }) => {
+const RemoveDatasetModal: React.FC<RemoveDatasetModalProps> = ({ username, name, onDsRemove, afterRemove }) => {
   const dispatch = useDispatch()
 
   const onRemove = () => {
     // TODO (ramfox): we are thinking of ways to adjust this syntax: https://github.com/qri-io/qrimatic/issues/98
+    onDsRemove()
     removeDataset({username, name})(dispatch)
       .then((action: AnyAction) => {
         if (action.type.includes('SUCCESS')) {
-          dispatch(loadCollection())
+          setTimeout(() => {
+            dispatch(removeCollectionItem(username, name))
+            dispatch(loadCollection())
+            afterRemove()
+          }, 800)
           return
         }
         // TODO (ramfox): when we add in toasts, this should be replaced with a
