1bda34ff6464122d80d76098f46c88195c7e53be
ramfox <kasey@qri.io>
2021-11-26 13:05:18 -0500

    fix(dsPreview): use `/ds/get` endpoint to get preview
    The `/ds/get` endpoint now returns a dataset preview (a dataset with the first 100 body rows inlined). We no longer need to fetch the dataset head and dataset body separately.

--- src/features/dsPreview/state/dsPreviewActions.ts
+++ src/features/dsPreview/state/dsPreviewActions.ts
@@ -2,27 +2,8 @@ import { QriRef } from "../../../qri/ref"
 import { ApiAction, ApiActionThunk, CALL_API } from "../../../store/api"
 
 export function loadDsPreview (ref: QriRef): ApiActionThunk {
-  return async (dispatch, getState) => {
-    dispatch({ type: 'DS_PREVIEW_REQUEST' })
-
-    try {
-      await Promise.all([
-        // for the moment we don't get a preview explicitly because we already
-        // have what we need in state.dataset
-        // dispatch(fetchDsPreview(ref)),
-        dispatch(fetchDsPreviewBody(ref))
-      ])
-
-      return dispatch({
-        type: 'DS_PREVIEW_SUCCESS',
-        ref
-      })
-    } catch (e: any) {
-      return dispatch({
-        type: 'DS_PREVIEW_FAILURE',
-        error: e.toString()
-      })
-    }
+  return async (dispatch) => {
+    return dispatch(fetchDsPreview(ref))
   }
 }
 
@@ -36,27 +17,6 @@ export function fetchDsPreview (ref: QriRef): ApiAction {
       endpoint: 'ds/get',
       method: 'GET',
       segments: ref
-    }
-  }
-}
-
-function fetchDsPreviewBody (ref: QriRef, page: number = 1, pageSize: number = bodyPageSizeDefault): ApiAction {
-  return {
-    type: 'previewbody',
-    ref,
-    [CALL_API]: {
-      endpoint: 'ds/get',
-      method: 'GET',
-      pageInfo: {
-        page,
-        pageSize
-      },
-      segments: {
-        username: ref.username,
-        name: ref.name,
-        path: ref.path,
-        selector: ['body']
-      }
     }
   }
 }
