785c9639a6b15fa30b3a33873b80814334ba5f2f
ramfox <kasey@qri.io>
2021-11-27 12:07:12 -0500

    refactor(UserProfile): adjust `NewUserProfile` to convert an object to a UserProfile

--- src/qri/userProfile.ts
+++ src/qri/userProfile.ts
@@ -1,7 +1,7 @@
 
 export interface UserProfile {
   profile_id: string
-  PrivKey: string
+  privKey: string
   username: string
   created: number
   updated: number
@@ -15,37 +15,53 @@ export interface UserProfile {
   photo: string
   poster: string
   twitter: string
-  PeerIDs: string[]
-  NetworkAddrs: string[]
+  peerIDs: string[]
+  networkAddrs: string[]
   id: string
   currentKey: string
-  EmailConfirmed: boolean
+  emailConfirmed: boolean
   isAdmin: boolean
 }
 
-export const NewUserProfile = () => {
+export const NewUserProfile = (d: Record<string, any>): UserProfile => {
+  let created = 0
+  let updated = 0
+
+  if (typeof d.created === 'string') {
+    const c = new Date(created)
+    created = c.getTime() / 1000
+  } else if (d.created) {
+    created = d.created
+  }
+
+  if (typeof d.updated === 'string') {
+    const u = new Date(updated)
+    updated = u.getTime() / 1000
+  } else if (d.updated) {
+    updated = d.updated
+  }
   return {
-    profile_id: '',
-    PrivKey: '',
-    username: '',
-    created: 0,
-    updated: 0,
-    type: '',
-    email: '',
-    name: '',
-    description: '',
-    home_url: '',
-    color: '',
-    thumb: '',
-    photo: '',
-    poster: '',
-    twitter: '',
-    PeerIDs: [],
-    NetworkAddrs: [],
-    id: '',
-    currentKey: '',
-    EmailConfirmed: false,
-    isAdmin: false
+    profile_id: d.id || d.profile_id || '',
+    privKey: d.privKey || d.PrivKey || '',
+    username: d.username || '',
+    created,
+    updated,
+    type: d.type || '',
+    email: d.email || '',
+    name: d.name || '',
+    description: d.description || '',
+    home_url: d.home_url || '',
+    color: d.color || '',
+    thumb: d.thumb || '',
+    photo: d.photo || '',
+    poster: d.poster || '',
+    twitter: d.twitter || '',
+    peerIDs: d.peerIDs || d.PeerIDs || [],
+    networkAddrs: d.networkAddrs || d.NetworkAddrs || [],
+    id: d.id || '',
+    currentKey: d.currentKey || '',
+    emailConfirmed: d.emailConfirmed || d.EmailConfirmed || '',
+    isAdmin: d.isAdmin || d.IsAdmin || false
   }
 }
 
